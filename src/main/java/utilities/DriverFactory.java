0b5ba85b62a676832724e4a8b690c5e518e116e0
Rahul23121995 <rahulnshanbag@rediffmail.com>
2026-05-09 17:16:07 +0530

    Fixed GitHub Actions Chrome execution

--- src/main/java/utilities/DriverFactory.java
+++ src/main/java/utilities/DriverFactory.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -11,7 +12,15 @@ public class DriverFactory {
     public static WebDriver initializeDriver() {
 
         WebDriverManager.chromedriver().setup();
-        tlDriver.set(new ChromeDriver());
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        tlDriver.set(new ChromeDriver(options));
 
         getDriver().manage().window().maximize();
 
